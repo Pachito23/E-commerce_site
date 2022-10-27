2b69d902833762053578d0a76891ec674ab614e0
Pachito23 <pasquale.vivaldo@student.upt.ro>
2022-10-28 01:05:42 +0300

    Dynamic cart + improved models and functionalities Dynamic rendering of cart + Back-end calculations of multiple parameters Parameters: prices, discounts prices, quantities, nr of elements in cart

--- store/models.py
+++ store/models.py
@@ -1,10 +1,12 @@
 # Create your models here.
 
+from datetime import datetime
 from email.policy import default
 from random import choices
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
+import datetime
 
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete = models.CASCADE, null = True, blank = True)
@@ -50,11 +52,48 @@ class Order(models.Model):
     def __str__(self):
         return str(self.id)
 
+    @property
+    def delivery_date1(self):
+        return self.date_ordered.date() + datetime.timedelta(days = 7)
+
+    @property
+    def delivery_date2(self):
+        return self.date_ordered.date() + datetime.timedelta(days = 14)
+
+
+    @property
+    def cart_total_elements(self):
+        orderitems = self.orderitem_set.all()
+        total = sum ([item.quantity for item in orderitems])
+        return total
+
+    @property
+    def cart_total_price(self):
+        orderitems = self.orderitem_set.all()
+        total = sum ([item.get_total_price for item in orderitems])
+        return total
+
+    @property
+    def cart_total_final_price(self):
+        orderitems = self.orderitem_set.all()
+        total = sum ([item.get_total_final_price for item in orderitems])
+        return total
+
 class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank = True, null = True)
     order = models.ForeignKey(Order, on_delete = models.SET_NULL, blank = True, null = True)
     quantity = models.IntegerField(default=0, null = True, blank = True)
     date_added = models.DateTimeField(auto_now_add = True)
+
+    @property
+    def get_total_final_price(self):
+        total = self.product.final_price() * self.quantity
+        return total
+
+    @property
+    def get_total_price(self):
+        total = self.product.price * self.quantity
+        return total
 
 class Shipping(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank = True, null = True)
